33ab7ec9c6e36a0f22666fa3fe6aeed87d230176
Daniel Streit <Daniel.Streit@gmail.com>
2014-08-22 12:26:02 -0700

    Add url variable

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var app = require('./server-config.js');
 
 var port = process.env.PORT || 4568;
+var url = process.env.URL || 'localhost';
 
-app.listen(port);
+app.listen(port, url);
 
 console.log('Server now listening on port ' + port);
